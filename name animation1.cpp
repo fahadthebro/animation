0c23ae15c967d7431af3120f9bf378defcca990a
fahad <109942134+fahadthebro@users.noreply.github.com>
2022-10-13 20:18:58 +0600

    Add files via upload

---
+++ name animation1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include<graphics.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    while(1){
+                cout<<"F"<<" ";
+                delay(1000);
+                cout<<"A"<<" ";
+                delay(1000);
+                cout<<"H"<<" ";
+                delay(1000);
+                cout<<"A"<<" ";
+                delay(1000);
+                cout<<"D"<<" ";
+                delay(1000);
+            }
+    }
+
+    getche();
+}
